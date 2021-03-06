f7b47753524be7bb77760500767dc156392c559e
cawoolf <rayadevinfo@gmail.com>
2021-03-05 19:51:12 -0700

    Notes Updated, SetList Activity

---
+++ app/src/main/java/com/rayadev/byoc/ui/main/SetListActivity.java
@@ -0,0 +1,16 @@
+package com.rayadev.byoc.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/*The Activity triggered by the user selection from the SetList
+Still has to relay data to a View Model like an other UI facing class.
+*/
+public class SetListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
