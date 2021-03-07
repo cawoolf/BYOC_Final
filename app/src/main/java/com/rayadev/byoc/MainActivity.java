e7d2d5db86a9883a7f09ba5860d88105fd94fb4e
cawoolf <rayadevinfo@gmail.com>
2021-03-07 12:49:54 -0700

    updates2.1

--- app/src/main/java/com/rayadev/byoc/MainActivity.java
+++ app/src/main/java/com/rayadev/byoc/MainActivity.java
@@ -4,9 +4,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
