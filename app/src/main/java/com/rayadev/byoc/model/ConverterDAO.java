248a0edd063d87f0364cdf856a2b9d27e85b3454
cawoolf <rayadevinfo@gmail.com>
2021-07-26 21:26:04 -0500

    repository

--- app/src/main/java/com/rayadev/byoc/model/ConverterDAO.java
+++ app/src/main/java/com/rayadev/byoc/model/ConverterDAO.java
@@ -1,20 +1,23 @@
 package com.rayadev.byoc.model;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
+import java.util.List;
+
 @Dao
 interface ConverterDAO {
 
     @Insert(onConflict = OnConflictStrategy.ABORT)
-    void insert();
+    void insert(Converter param);
 
     @Delete
     void delete(Converter converter);
 
-    @Query("SELECT * from converter_table")
-    void getFavoriteConverterList();
+    @Query("SELECT * from converter_table") //Maybe try to order by primary key, or make it so that you can drag and rearrange the order of the converters.
+    LiveData<List<Converter>> getFavoriteConverterList();
 }
--- app/src/main/java/com/rayadev/byoc/model/ConverterRepository.java
+++ app/src/main/java/com/rayadev/byoc/model/ConverterRepository.java
@@ -1,5 +1,8 @@
 package com.rayadev.byoc.model;
 
+import android.app.Application;
+import android.os.AsyncTask;
+
 import androidx.lifecycle.LiveData;
 
 import java.util.List;
@@ -8,6 +11,55 @@ public class ConverterRepository {
 
     private ConverterDAO mConverterDAO;
     LiveData<List<Converter>> mConverterFavoritesList;
+
+    //Add a constructor that gets a handle to the database and initializes the member variables.
+    public ConverterRepository(Application application) {
+        ConverterDatabase db = ConverterDatabase.getDatabase(application);
+        mConverterDAO = db.getConverterDAO();
+        mConverterFavoritesList = mConverterDAO.getFavoriteConverterList();
+    }
+
+    //All accessible methods
+    public LiveData<List<Converter>> getConverterFavoritesList() {return  mConverterFavoritesList;}
+
+    public void insert (Converter converter) {
+        new insertAsyncTask(mConverterDAO).execute(converter);
+    }
+
+    public void delete (Converter converter) {
+        new deleteAsyncTask(mConverterDAO).execute(converter);}
+
+    //AsyncTasks for database methods.
+    private static class insertAsyncTask extends AsyncTask<Converter, Void, Void> {
+
+        private final ConverterDAO mAsyncTaskDao;
+
+        insertAsyncTask(ConverterDAO dao) {
+            mAsyncTaskDao = dao;
+        }
+
+        @Override
+        protected Void doInBackground(final Converter... params) {
+            mAsyncTaskDao.insert(params[0]);
+            return null;
+        }
+    }
+
+    private static class deleteAsyncTask extends AsyncTask<Converter, Void, Void> {
+
+        private final ConverterDAO mAsyncTaskDao;
+
+         deleteAsyncTask(ConverterDAO asyncTaskDao) {
+            mAsyncTaskDao = asyncTaskDao;
+        }
+
+        @Override
+        protected Void doInBackground(final Converter... converters) {
+
+            mAsyncTaskDao.delete(converters[0]);
+            return null;
+        }
+    }
 
 
 }
