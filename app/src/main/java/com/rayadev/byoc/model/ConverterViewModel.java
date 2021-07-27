780f2fe445a26c352d72714432fb601842484003
cawoolf <rayadevinfo@gmail.com>
2021-07-26 21:32:36 -0500

    set up almost done

---
+++ app/src/main/java/com/rayadev/byoc/model/ConverterViewModel.java
@@ -0,0 +1,44 @@
+package com.rayadev.byoc.model;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class ConverterViewModel extends AndroidViewModel {
+    //Add a private member variable to hold a reference to the Repository.
+    private ConverterRepository mRepository;
+
+    //Add a private LiveData member variable to cache the list of words.
+    private LiveData<List<Converter>> mConverterFavoritesList;
+
+    public ConverterViewModel(@NonNull Application application) {
+        super(application);
+        mRepository = new ConverterRepository(application);
+        mConverterFavoritesList = mRepository.getConverterFavoritesList();
+    }
+
+    // The ViewModel implements all methods that will be available to the Main Activity.
+    // This completely hides the implementation from the UI.
+    // Pass these up the the Main Activity or whatever needs access.
+
+    //Wrapper for the "getter" method that gets all the words.
+    LiveData<List<Converter>> getConverterFavoritesList() {
+        return mConverterFavoritesList;
+    }
+
+    //The insert method wrapper.
+    public void insert(Converter converter) {
+        mRepository.insert(converter);
+    }
+
+    //Deletes a single word from the database
+    public void delete(Converter converter) {
+        mRepository.delete(converter);
+    }
+}
