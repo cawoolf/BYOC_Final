c86429942832ac3dc09ec65af64db0f80cf00267
cawoolf <rayadevinfo@gmail.com>
2021-08-03 20:56:58 -0500

    currency api

--- app/src/main/java/com/rayadev/byoc/MainActivity.java
+++ app/src/main/java/com/rayadev/byoc/MainActivity.java
@@ -5,18 +5,35 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
+import com.rayadev.byoc.model.CurrencyAPI;
 import com.rayadev.byoc.ui.main.PageAdapter;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CurrencyAPI mCurrencyAPI;
+    private String mCurrencyPair;
+    private final String mAPIKey = "882cc2509c2a6546a18c";
+    private final String TAG = "ATAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +54,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadCurrencyData() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://free.currconv.com/api/v7/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mCurrencyAPI = retrofit.create(CurrencyAPI.class);
+
         HashMap<String, Double> currencyPairs = new HashMap<>();
         String[] currencies = {getString(R.string.currency_USD), getString(R.string.currency_CAD),
                 getString(R.string.currency_EUR), getString(R.string.currency_NZD)};
 
 
-
     }
+
+
 
     private void keyBoardManager() {
 
@@ -118,6 +143,58 @@ public class MainActivity extends AppCompatActivity {
 
 
         return super.onCreateOptionsMenu(menu);
+    }
+
+
+
+    private void getCurrency() {
+
+
+//        Call call = mCurrencyAPI.getCurrencyPair(pair);
+        Call call = mCurrencyAPI.getCurrency(getUrlString(mAPIKey, mCurrencyPair));
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(MainActivity.this, "Code: " + response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+//                String code = "Success Code:" + response.code();
+//                String body = new Gson().toJson(response.body());
+//                textViewResult.setText(code + "\n" + body);
+
+                String body = new Gson().toJson(response.body());
+                JSONObject currency = null;
+                try {
+                    currency = new JSONObject(body);
+
+                    String result = "Code: " + response.code() +"\n" +
+                            mCurrencyPair + ": "+ currency.getString(mCurrencyPair);
+
+                    Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+
+
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+
+                String fail = "Fail"+ t.getMessage();
+                Log.i(TAG, fail);
+
+            }
+        });
+
+    }
+
+    private String getUrlString(String apiKey, String currencyPair) {
+        return "https://free.currconv.com/api/v7/convert?q=" + currencyPair + "&compact=ultra&apiKey=" + apiKey;
     }
 
 }
