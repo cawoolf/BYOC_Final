daea60a452008e166475a492e95ba5487b6441ec
cawoolf <rayadevinfo@gmail.com>
2021-08-29 18:20:12 -0500

    custom layout stuff

--- app/src/main/java/com/rayadev/byoc/ui/main/CustomConverterActivity.java
+++ app/src/main/java/com/rayadev/byoc/ui/main/CustomConverterActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.rayadev.byoc.R;
 
@@ -14,11 +20,21 @@ Allows the user to build their own converter, and save it to the HomeTab.
 
 public class CustomConverterActivity extends AppCompatActivity {
 
+    //Views for the converter UI
+    private TextView mUnitATitleTextView, mUnitBTitleTextView;
+    private EditText mUnitAInputEditText, mUnitBInputEditText;
+    private ImageButton mConverterInfoButton, mConverterSwapButton;
+
+    //Bottom UI
+    private ImageView mAddConverterButton;
+    private LinearLayout mBottomUI;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_converter);
         setUpToolbar();
+        linkViews();
     }
 
     private void setUpToolbar() {
@@ -29,5 +45,19 @@ public class CustomConverterActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         setTitle("Converter Workshop");
+    }
+
+    private void linkViews() {
+        //Link ConverterBox Views
+        View converterUILayout = findViewById( R.id.custom_converter_cardlayout_include_converter_tab ); // root View id from include
+
+        mUnitATitleTextView = converterUILayout.findViewById(R.id.cardView_UnitATitle_TextView);
+        mUnitBTitleTextView = converterUILayout.findViewById(R.id.cardView_UnitBTitle_TextView);
+
+        mUnitAInputEditText = converterUILayout.findViewById(R.id.cardView_UnitAInput_EditText);
+        mUnitBInputEditText = converterUILayout.findViewById(R.id.cardView_UnitBInput_EditText);
+
+        mConverterInfoButton = converterUILayout.findViewById(R.id.cardView_InfoButton);
+        mConverterSwapButton = converterUILayout.findViewById(R.id.cardView_SwapButton);
     }
 }
